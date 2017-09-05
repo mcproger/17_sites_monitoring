ecc3248f558f2b46cda70dfc9091ddd7760e4a05
mcproger <artem7lomonosov@gmail.com>
2017-09-05 16:02:43 +0300

    Add argparse function and load urls list function

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,22 @@
+import requests
+import os
+
+
 def load_urls4check(path):
-    pass
+    if not os.path.exists(path):
+        return None
+    with open(path, mode='r', encoding='utf-8') as file_handler:
+        urls_for_check = file_handler.read()
+        return urls_for_check
+
 
 def is_server_respond_with_200(url):
     pass
 
+
 def get_domain_expiration_date(domain_name):
     pass
+
 
 if __name__ == '__main__':
     pass
